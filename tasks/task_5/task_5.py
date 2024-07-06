9afa7ef2e07919e32a2513eb01e592b0c665df6c
Bhanu Prakash Putta <bhanu.putta1999@gmail.com>
2024-07-06 10:01:11 -0400

    Implemented Task 5

--- tasks/task_5/task_5.py
+++ tasks/task_5/task_5.py
@@ -11,6 +11,10 @@ from langchain_core.documents import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 
+import chromadb
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../../authentication.json"
+
 class ChromaCollectionCreator:
     def __init__(self, processor, embed_model):
         """
@@ -58,6 +62,16 @@ class ChromaCollectionCreator:
         # https://python.langchain.com/docs/modules/data_connection/document_transformers/character_text_splitter
         # [Your code here for splitting documents]
         
+        text_splitter = CharacterTextSplitter(
+            separator="\n\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+            is_separator_regex=False,
+        )
+
+        texts = text_splitter.split_documents(self.processor.pages)
+
         if texts is not None:
             st.success(f"Successfully split pages to {len(texts)} documents!", icon="✅")
 
@@ -65,7 +79,10 @@ class ChromaCollectionCreator:
         # https://docs.trychroma.com/
         # Create a Chroma in-memory client using the text chunks and the embeddings model
         # [Your code here for creating Chroma collection]
-        
+        client = chromadb.EphemeralClient()
+        self.db = Chroma.from_documents(texts, self.embed_model, client=client, collection_name="mission_quizzifY")
+
+
         if self.db:
             st.success("Successfully created Chroma Collection!", icon="✅")
         else:
@@ -93,7 +110,7 @@ if __name__ == "__main__":
     
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "YOUR PROJECT ID HERE",
+        "project": "gemini-quizzify-427417",
         "location": "us-central1"
     }
     
